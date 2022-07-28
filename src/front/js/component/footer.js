2bfeed2aae18ff840c499376dbf757db595257fc
hammerToFall <lenorka0@hotmail.com>
2022-07-28 17:21:11 +0000

    cambio footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-  <footer class="navbar navbar-light bg-dark">
-    <div class="container-fluid">
-      <span class="navbar-text text-light">© Black Box 2022</span>
-      <span class="footer-text text-light">
-        <i class="fab fa-instagram fa-lg mx-1"></i>
-        <i class="fab fa-facebook fa-lg mx-1"></i>
-        <i class="fab fa-twitter-square fa-lg mx-1"></i>
-        <i class="fab fa-pinterest-square fa-lg ms-1 me-3"></i>
+  <footer className="navbar navbar-light bg-dark">
+    <div className="container-fluid">
+      <span className="navbar-text text-light">© Black Box 2022</span>
+      <span className="footer-text text-light">
+        <i className="fab fa-instagram fa-lg mx-1"></i>
+        <i className="fab fa-facebook fa-lg mx-1"></i>
+        <i className="fab fa-twitter-square fa-lg mx-1"></i>
+        <i className="fab fa-pinterest-square fa-lg ms-1 me-3"></i>
       </span>
     </div>
   </footer>
