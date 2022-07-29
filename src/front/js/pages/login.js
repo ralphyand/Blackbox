58fa11ee4ef93829b5e1c36b2701004d76432774
hammerToFall <lenorka0@hotmail.com>
2022-07-29 10:57:47 +0000

    pagina de login

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -2,38 +2,43 @@ import React from "react";
 
 export const Login = () => {
   return (
-    <div className="container-fluid">
-      <div className="login row rounded justify-content-center py-2">
-        <div className="imgPart col-md-5">
-          <img
-            src="https://images.unsplash.com/photo-1590486803833-1c5dc8ddd4c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-            className="img-fluid border border-light"
-            alt="..."
-          />
-        </div>
-        <div className="inputPart col-md-5 border border-light pt-2">
-          <div className="form-floating mb-3">
-            <input
-              type="email"
-              className="form-control"
-              id="floatingInput"
-              placeholder="name@example.com"
+    <div className="container-fluid my-5 pt-5">
+      <div className="container my-3">
+        <div className="login row rounded justify-content-center py-3">
+          <p className="row justify-content-center text-light">
+            Inicio de sesión
+          </p>
+          <div className="imgPart col-md-5">
+            <img
+              src="https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Zm90b2dyYWYlQzMlQURhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+              className="img-fluid border border-light"
+              alt="..."
             />
-            <label htmlFor="floatingInput">Correo</label>
           </div>
-          <div className="form-floating">
-            <input
-              type="password"
-              className="form-control"
-              id="floatingPassword"
-              placeholder="Password"
-            />
-            <label htmlFor="floatingPassword">Contraseña</label>
-          </div>
-          <div className="d-flex justify-content-center">
-            <button type="button" className="btn btn-dark rounded-pill">
-              Iniciar sesión
-            </button>
+          <div className="inputPart col-md-5 border border-light pt-2">
+            <div className="form-floating mb-3">
+              <input
+                type="email"
+                className="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+              />
+              <label htmlFor="floatingInput">Correo</label>
+            </div>
+            <div className="form-floating">
+              <input
+                type="password"
+                className="form-control"
+                id="floatingPassword"
+                placeholder="Password"
+              />
+              <label htmlFor="floatingPassword">Contraseña</label>
+            </div>
+            <div className="d-flex justify-content-center mt-5">
+              <button type="button" className="btn btn-dark rounded-pill">
+                Iniciar sesión
+              </button>
+            </div>
           </div>
         </div>
       </div>
