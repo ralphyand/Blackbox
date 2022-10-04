f5bc8fb0998200e4ed8d2922d96c09eaba9b17f8
hammerToFall <lenorka0@hotmail.com>
2022-10-04 16:25:14 +0000

    actualizo la ruta de user

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,11 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User ,Course, Teacher, Compras ,Temario, Video,Category
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+import json
+
+import stripe
+# This is your test secret API key.
+stripe.api_key = "sk_test_51LVGdOAWxURsmnxVwoStl969rbcwiP1kGosBvclfsES9CTedRu0rXVdyiAqpDx8DJnXx47bs43o3kfDhKgw6KlC100onZoxswg"
 
 api = Blueprint('api', __name__)
 
@@ -12,6 +17,7 @@ api = Blueprint('api', __name__)
 def create_user():
     data = request.json
     user = User(name=data.get('name'), lastname=data.get('lastname'), email=data.get('email'), password=data.get('password'))
+
     db.session.add(user)
     db.session.commit()
 
@@ -48,13 +54,17 @@ def get_cursos() :
 
 
 @api.route('/course/<int:id>', methods=['GET'])
+@jwt_required(optional=True)
 def course_detail(id) :
+    user_id = get_jwt_identity()
     course = Course.query.get(id)
-    return jsonify(course.serialize()),200
+    data = course.serialize()
+    data["is_paid"]= False   
+    if user_id : 
+        compra = Compras.query.filter_by(user_id= user_id,course_id=id).first() 
+        data["is_paid"]= True if compra else False   
+    return jsonify(data),200
 
-
-
-   
 
 @api.route('/user', methods=['GET'])
 def get_users() :
@@ -74,7 +84,6 @@ def get_teachers() :
            data.append(teacher.serialize())
 
        return jsonify(data),200
-
 
 
 @api.route('/temario', methods=['GET'])
@@ -114,3 +123,72 @@ def get_comprados() :
             data.append(compras.serialize())
 
         return jsonify(data),200
+
+
+
+def calculate_order_amount(items):
+    # Replace this constant with a calculation of the order's amount
+    # Calculate the order total on the server to prevent
+    # people from directly manipulating the amount on the client
+    stripeId = items[0]['id']
+    course = Course.query.filter_by(codigodepago_id=stripeId).first()
+    return int(course.price * 100)
+
+@api.route('/create-payment-intent', methods=['POST'])
+def create_payment():
+    try:
+        data = json.loads(request.data)
+        intent = stripe.PaymentIntent.create(
+            amount=calculate_order_amount(data['items']),
+            currency='usd'
+        )
+
+        return jsonify({
+          'clientSecret': intent['client_secret']
+        })
+    except Exception as e:
+        return jsonify(error=str(e)), 403       
+
+
+@api.route('/private-course', methods=['GET'])
+@jwt_required() # Restringimos acceso a la API
+def get_private_course():
+    user_id = get_jwt_identity()
+    compras = Compras.query.filter_by(user_id=user_id)
+    course = [compra.course.serialize() for compra in compras]
+    return jsonify(course), 200
+
+#ruta de compra 
+
+@api.route('/compra', methods=['POST'])
+@jwt_required()
+def compra_user():
+    user_id = get_jwt_identity()
+    data = request.json
+    compra = Compras(user_id = user_id, price = data.get("price"), course_id = data.get("course_id") )
+    db.session.add(compra)
+    db.session.commit()
+
+    return jsonify({"message": "compra realizada"}), 200
+
+
+@api.route('/user_profile', methods=['PUT'])
+@jwt_required()
+def editUser():
+    user_id = get_jwt_identity()
+    data = request.json
+    user = User.query.get(user_id)   
+    user.name = data.get("name")
+    user.lastname = data.get("lastname")
+    user.email = data.get("email")
+    user.password = data.get("password")
+
+    db.session.commit()
+
+    return jsonify({"message": "user_profile updated"}), 200
+
+
+
+
+ 
+
